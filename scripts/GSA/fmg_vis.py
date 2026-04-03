522ab67176658a28e93e9ad3bc201dabedaeceff
armankhoshnevis <khoshne1@msu.edu>
2026-04-03 16:12:17 -0400

    Add main scripts for sensitivity analysis and visualization of FMG model

---
+++ scripts/GSA/fmg_vis.py
@@ -0,0 +1,68 @@
+import json
+import argparse
+import numpy as np
+
+from fmg_gsa_vis_utils import (
+    plot_sensitivity_indices,
+    read_excel_range,
+    plot_Linf_grouped
+)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--S', type=str, help='Sensitivity index type (S1 or ST)')
+    parser.add_argument('--E', type=str, help='Modulus type (Ep, Epp, or Ecomplex)')
+    parser.add_argument('--N', type=int, help='Number of samples (N = 2**m)')
+    parser.add_argument('--HS', type=int, default=20)
+    parser.add_argument('--GnP_idx', type=int, default=0, help='Index of GnP in GnP_list for which to perform sensitivity analysis. GnP_idx = 0, 1, 2, 3 -> 0%, 0.5%, 1%, and 1.5% GnP')
+    args = parser.parse_args()
+    
+    w_freq = np.logspace(-8, 2, 100)
+    
+    with open(f'../../configs/{args.HS}HSWF_FMG_Config.json', 'r') as config_file:
+        config = json.load(config_file)
+    
+    GnP_list = config['GnP_list']
+    params_list = config['params_list']
+    file_path = config['file_path']
+    
+    plot_sensitivity_indices(
+        S=args.S,
+        E=args.E,
+        N=args.N,
+        HS=args.HS,
+        w_freq=w_freq,
+        GnP_list=GnP_list,
+        params_list=params_list,
+        file_path=file_path
+    )
+
+    filename = "../../results/GSA/GSA_FMG.xlsx"
+
+    Linf_Ep_20HS  = read_excel_range(filename=filename, cell_range="C8:H9")
+    Linf_Ep_30HS  = read_excel_range(filename=filename, cell_range="C15:H16")
+    Linf_Ep_40HS  = read_excel_range(filename=filename, cell_range="C22:H23")
+
+    Linf_Epp_20HS = read_excel_range(filename=filename, cell_range="L8:Q9")
+    Linf_Epp_30HS = read_excel_range(filename=filename, cell_range="L15:Q16")
+    Linf_Epp_40HS = read_excel_range(filename=filename, cell_range="L22:Q23")
+
+    Linf_Ecomplex_20HS = read_excel_range(filename=filename, cell_range="U8:Z9")
+    Linf_Ecomplex_30HS = read_excel_range(filename=filename, cell_range="U15:Z16")
+    Linf_Ecomplex_40HS = read_excel_range(filename=filename, cell_range="U22:Z23")
+
+    Linf_Ep_mean = np.vstack([Linf_Ep_20HS[0, :],  Linf_Ep_30HS[0, :],  Linf_Ep_40HS[0, :]])
+    Linf_Ep_std = np.vstack([Linf_Ep_20HS[1, :],  Linf_Ep_30HS[1, :],  Linf_Ep_40HS[1, :]])
+
+    Linf_Epp_mean = np.vstack([Linf_Epp_20HS[0, :], Linf_Epp_30HS[0, :], Linf_Epp_40HS[0, :]])
+    Linf_Epp_std = np.vstack([Linf_Epp_20HS[1, :], Linf_Epp_30HS[1, :], Linf_Epp_40HS[1, :]])
+
+    Linf_Ecomplex_mean = np.vstack([Linf_Ecomplex_20HS[0, :], Linf_Ecomplex_30HS[0, :], Linf_Ecomplex_40HS[0, :]])
+    Linf_Ecomplex_std = np.vstack([Linf_Ecomplex_20HS[1, :], Linf_Ecomplex_30HS[1, :], Linf_Ecomplex_40HS[1, :]])
+
+    plot_Linf_grouped(
+        Linf_Ep_mean,
+        Linf_Ep_std,
+        ylabel=r"$||\bar{S}_{E^{\prime}}||_{L_{\infty}}$",
+        save_path="../../results/GSA/GSA_Linf_Ep"
+    )
